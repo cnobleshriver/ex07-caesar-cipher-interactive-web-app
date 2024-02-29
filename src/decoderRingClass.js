b6e3a14a33d7572041f7d80b674a094ba2a49d77
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-02-29 16:02:50 -0500

    first commit

---
+++ src/decoderRingClass.js
@@ -0,0 +1,65 @@
+// decoder is a base class that operates on a substitution cipher the input to
+// the constructor is the substitution string, which should contain all
+// lower-case letters in some order. e.g., let d = new
+// decoder('nopqrstuvwxyzabcdefghijklm'); implements ROT13.
+
+class decoder {
+  constructor(cipher) {
+    this._cipher = cipher;
+    this._rebuildMaps();
+  }
+
+  get cipher() {
+    return this._cipher;
+  }
+  _rebuildMaps() {
+    this._encodeMap = {};
+    this._decodeMap = {};
+    this._cipher.split("").forEach((ch, index) => {
+      this._encodeMap[String.fromCharCode(index + 97)] = ch;
+      this._decodeMap[ch] = String.fromCharCode(index + 97);
+    });
+  }
+
+  set cipher(newcipher) {
+    this._cipher = newcipher;
+    this._rebuildMaps();
+  }
+
+  encode(str) {
+    let s = str.split("").map((x) => {
+      if (x in this._encodeMap) {
+        return this._encodeMap[x];
+      } else {
+        return x;
+      }
+    });
+    return s.join("");
+  }
+
+  decode(str) {
+    let s = str.split("").map((x) => {
+      if (x in this._decodeMap) {
+        return this._decodeMap[x];
+      } else {
+        return x;
+      }
+    });
+    return s.join("");
+  }
+}
+
+// now the decoderRing is just a subclass of decoder that initializes it with a
+// rotated string
+class decoderRing extends decoder {
+  constructor(rotation) {
+    let chars = [];
+    for (let i = 0; i < 26; i++) {
+      chars.push(String.fromCharCode(97 + ((i + rotation) % 26)));
+    }
+    super(chars.join(""));
+  }
+}
+
+// TASK #2: export the decoder class
+
---
+++ src/interactive.js
@@ -0,0 +1,49 @@
+// TASK #2: import the decoder class
+
+
+// Create a decoder object.
+const d = new decoder("");
+
+// TASK #3: Validate the cipher key text input
+function validateKeyTextInput() {
+  
+  /* BEGIN_STUDENT
+  throw new Error('Function not implemented');
+  END_STUDENT */
+}
+
+// TASK #3:  Color the encoded text box.
+function colorEncodedTextBox() {
+  
+  /* BEGIN_STUDENT
+  throw new Error('Function not implemented');
+  END_STUDENT */
+}
+
+// TASK #3: Encode the text in the encode text box.
+function encodeText() {
+  
+  /* BEGIN_STUDENT
+  throw new Error('Function not implemented');
+  END_STUDENT */
+}
+
+// TASK #3: Color the decoded text box.
+function colorDecodedTextBox() {
+  
+  /* BEGIN_STUDENT
+  throw new Error('Function not implemented');
+  END_STUDENT */
+}
+
+// TASK #3: Decode the text in the decode text box.
+function decodeText() {
+  
+  /* BEGIN_STUDENT
+  throw new Error('Function not implemented');
+  END_STUDENT */
+}
+
+// TASK #4: Assign event handlers to events. Initialize background color for key
+// input.
+
