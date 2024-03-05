d94c1c329ac5a5ddc0db39815cfa6cb2050254c6
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-03-04 22:06:12 -0500

    all tests pass

--- src/interactive.js
+++ src/interactive.js
@@ -6,53 +6,61 @@ const d = new decoder("");
 
 // TASK #3: Validate the cipher key text input
 function validateKeyTextInput() {
-  console.log("Validating key text input... \n")
   const key = document.getElementById("key").value;
   if (key.length === 26 && key.toLowerCase() === key) {
-    // make white
-    console.log("keyTextBox is white");
+    document.getElementById("key").style.backgroundColor = "white";
+    d.cipher = key;
   } else {
-    // make yellow
-    console.log("keyTextBox is yellow");
+    document.getElementById("key").style.backgroundColor = "yellow";
   }
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
 }
 
 // TASK #3:  Color the encoded text box.
 function colorEncodedTextBox() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
+  const encodedTextBox = document.getElementById("encoded");
+  if (encodedTextBox.value === "") {
+    encodedTextBox.style.backgroundColor = "white";
+    encodedTextBox.style.color = "black";
+  } else {
+    encodedTextBox.style.backgroundColor = "red";
+    encodedTextBox.style.color = "white";
+  }
 }
 
 // TASK #3: Encode the text in the encode text box.
 function encodeText() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
+  const input = document.getElementById("encode").value;
+  const encoded = d.encode(input);
+  document.getElementById("encoded").value = encoded;
+  colorEncodedTextBox();
 }
 
 // TASK #3: Color the decoded text box.
 function colorDecodedTextBox() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
+  const decodedTextBox = document.getElementById("decoded");
+  if (decodedTextBox.value === "") {
+    decodedTextBox.style.backgroundColor = "white";
+    decodedTextBox.style.color = "black";
+  } else {
+    decodedTextBox.style.backgroundColor = "green";
+    decodedTextBox.style.color = "white";
+  }
 }
 
 // TASK #3: Decode the text in the decode text box.
 function decodeText() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
+  const input = document.getElementById("decode").value;
+  const decoded = d.decode(input);
+  document.getElementById("decoded").value = decoded;
+  colorDecodedTextBox();
 }
 
 // TASK #4: Assign event handlers to events. Initialize background color for key
 // input.
-
+document.getElementById("key").style.backgroundColor = "yellow";
+document.getElementById("key").addEventListener("keyup", validateKeyTextInput);
+document.getElementById("encode").addEventListener("keyup", encodeText);
+document.getElementById("encode").addEventListener("keyup", encodeText);
+document.getElementById("encode").addEventListener("keyup", colorEncodedTextBox);
+document.getElementById("decode").addEventListener("keyup", decodeText);
+document.getElementById("decode").addEventListener("keyup", colorDecodedTextBox);
